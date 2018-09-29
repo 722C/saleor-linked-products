8f251dd2efe063daa2a7b1f955b9f80d5821e027
722C <722C@users.noreply.github.com>
2018-09-28 18:26:09 -0600

    Add template tag for displaying linked products.

--- linked_products/templatetags/linked_products.py
+++ linked_products/templatetags/linked_products.py
@@ -1,9 +1,41 @@
 from django import template
 
+from saleor.product.utils import products_with_availability
+
 register = template.Library()
 
 
 @register.inclusion_tag('linked_products/dashboard/side_nav_inclusion.html',
                         takes_context=True)
 def linked_products_side_nav(context):
+    return context
+
+
+@register.inclusion_tag('linked_products/group.html', takes_context=True)
+def linked_products(context, product):
+    request = context.get('request', None)
+    linked_product = product.links.first()
+    context.update({
+        'linked_product': linked_product
+    })
+
+    if linked_product and request:
+        products = list(filter(lambda product: product,
+                               linked_product.products.all()))
+        if len(products) > 1:
+            longest_common_anchored_substring = ''
+            for first, second in zip(products[0].name, products[1].name):
+                if first == second:
+                    longest_common_anchored_substring += first
+                else:
+                    break
+            products_and_availability = list(products_with_availability(
+                products, request.discounts, request.taxes,
+                request.currency))
+            context.update({
+                'linked_product_products': products_and_availability,
+                'linked_product_to_cut_from_name':
+                longest_common_anchored_substring,
+            })
+
     return context
