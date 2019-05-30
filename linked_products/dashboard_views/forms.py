826de375cdb1f1a4d0eefbc485be38545b7fb03e
ryan28561 <ryan@lightningkite.com>
2019-05-30 11:01:58 -0600

    only allow products to be in one set of links

--- linked_products/dashboard_views/forms.py
+++ linked_products/dashboard_views/forms.py
@@ -1,14 +1,26 @@
 from django import forms
-from django.db.models import Q
+from django.db.models import Q, Count
 from django.utils.translation import pgettext_lazy
 
-from saleor.product.models import Category
+from saleor.product.models import Category, Product
 from ..models import LinkedProduct
 
 
 class LinkedProductForm(forms.ModelForm):
+    
+    products = forms.ModelMultipleChoiceField(queryset=Product.objects.all())
 
     class Meta:
         model = LinkedProduct
         verbose_name_plural = 'linked products'
         exclude = []
+
+    def __init__(self, *args, **kwargs):
+        super(forms.ModelForm, self).__init__(*args, **kwargs)
+        instance = kwargs.get('instance', None)
+        if instance:
+            self.fields['products'].queryset = Product.objects.annotate(link_count=Count('links')).filter(Q(link_count=0) | Q(links=instance))
+        else:
+            self.fields['products'].queryset = Product.objects.annotate(link_count=Count('links')).filter(link_count=0)
+
+
